080e62dcc5067e6f34dbd1358f0c36995752c372
Emmanuel, Oluwatobi <oluwatobiemmanuel@my.unt.edu>
2025-06-12 12:12:36 -0500

    Read dataset using Pandas

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
+df = pd.read_csv("./dictionary.csv")
 
 @app.route("/")
 def home():
@@ -10,9 +12,13 @@ def home():
 
 @app.route("/api/v1/<word>")
 def dest(word):
-    return {"Word": str(word),
-            "Definition": str(word).upper()
+
+    definition = df.loc[df["word"] == word]["definition"].squeeze()
+
+    dictionary_output = {"Word": str(word).capitalize(),
+            "Definition": str(definition)
             }
+    return dictionary_output
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
